d48590a90db4b6d64a4c47d6ad25109f1ededac8
kongzx <396864706@qq.com>
2016-11-15 19:57:30 +0800

    check-all

---
+++ src/directive/check-all.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+const handler = function (){
+
+    
+}
+
+ Vue.directive('check-all' , {
+        bind: function (el,binding) {
+           
+        },
+        update: function (el,binding) {
+            
+        }
+    }
+) 
